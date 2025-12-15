61cc28f47522ebef5dfd50e45608e1fb98b9c361
K.Shivananda Dora <shivanandadora@gmail.com>
2025-12-15 22:18:46 +0530

    Fix production error: currUser undefined and session handling

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const store = MongoStore.create({
     touchAfter: 24 * 3600,
 });
 
-store.on("error",()=>{
+store.on("error",(err)=>{
     console.log("ERROR in MONGO SESSION STORE",err);
 });
 
@@ -45,7 +45,7 @@ const sessionOptions = {
     store,
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
